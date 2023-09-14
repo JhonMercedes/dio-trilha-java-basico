f66a3bd0f5cea148c0e868947db55fe27b48cb80
JhonMercedes <jhonzeras93@gmail.com>
2023-09-14 01:41:07 -0300

    condiçaõ ternaria

--- resultado-escolar/src/ResultadoEscolar.java
+++ resultado-escolar/src/ResultadoEscolar.java
@@ -1,14 +1,24 @@
 public class ResultadoEscolar {
     public static void main(String[] args) throws Exception {
-        int nota = 6;
+        int nota = 3;
 
-        if(nota >= 7) 
-            System.out.println("Aprovado");
+        // if(nota >= 7) 
+        //     System.out.println("Aprovado");
 
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Recuperação");
+        // else if (nota >= 5 && nota < 7)
+        //     System.out.println("Recuperação");
             
-        else 
-        System.out.println("Reprovado");
-    }
+        // else 
+        // System.out.println("Reprovado");
+
+        // CONDIÇÕES TERNARIAS EX 1 
+
+        //String resulatdo = nota >=7 ? "Aprovado" : "Reprovado";
+        //System.out.println(resultado);
+
+        // CONDIÇÕES TERNARIAS EX 2
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperacao" : "Reprovado";
+        System.out.println(resultado);
+        }
 }
