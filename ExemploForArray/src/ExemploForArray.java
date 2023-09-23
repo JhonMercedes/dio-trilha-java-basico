731ccd2adf9af75777784f1e61864e3db9078cda
JhonMercedes <jhonzeras93@gmail.com>
2023-09-23 08:06:59 -0300

    estrutura de repeticção

---
+++ ExemploForArray/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    
+    public static void main (String[] args) {
+
+        String alunos [] = {"FELIPE", "JHON", "JULIA", "MARCOS"};
+
+        for (int x=0; x < alunos.length; x++){
+            System.out.println("O aluno no indice =x "+x+ " e " + alunos [ x] );
+        }
+        
+    }
+}
